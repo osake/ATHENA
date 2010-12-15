8397fde0f87c4774c8fbbec3a3c2a987f17421b8
Fintan Donaghy <fdonaghy@gmail.com>
2010-12-15 19:35:58 +0000

    renamed Array serializer

--- core/audit/src/main/java/org/fracturedatlas/athena/audit/serialization/AuditMessageArraySerializer.java
+++ core/audit/src/main/java/org/fracturedatlas/athena/audit/serialization/AuditMessageArraySerializer.java
@@ -7,14 +7,10 @@ package org.fracturedatlas.athena.audit.serialization;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.LongSerializationPolicy;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Collection;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -23,10 +19,9 @@ import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 import org.fracturedatlas.athena.audit.model.AuditMessage;
 
-//HACKL This is to disable auditing because fo the serialization problem
-//@Provider
+@Provider
 @Produces({"application/json"})
-public class AuditMessageCollectionSerializer implements MessageBodyWriter<AuditMessage[]> {
+public class AuditMessageArraySerializer implements MessageBodyWriter<AuditMessage[]> {
 
     @Override
     public long getSize(AuditMessage[] t, Class<?> type, Type type1, Annotation[] annotations, MediaType mediaType) {
