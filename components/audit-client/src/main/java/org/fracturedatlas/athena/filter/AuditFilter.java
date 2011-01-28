cd4abe985bbe465966ab84c882371a598a1b330f
Gary M <gary.moore@gmail.com>
2011-01-27 20:58:38 -0500

    Added props file to audit-client.  Made it so one can configure the scrubbed props via the peopls file

--- components/audit-client/src/main/java/org/fracturedatlas/athena/filter/AuditFilter.java
+++ components/audit-client/src/main/java/org/fracturedatlas/athena/filter/AuditFilter.java
@@ -6,14 +6,7 @@ package org.fracturedatlas.athena.filter;
 
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerRequestFilter;
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import com.google.gson.Gson;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.Client;
@@ -22,17 +15,16 @@ import com.sun.jersey.api.client.config.DefaultClientConfig;
 import com.sun.jersey.core.util.ReaderWriter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.fracturedatlas.athena.client.audit.PublicAuditMessage;
 import org.fracturedatlas.athena.util.Scrubber;
 import org.fracturedatlas.athena.web.util.JsonUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
 
 public class AuditFilter implements ContainerRequestFilter {
 
@@ -41,29 +33,24 @@ public class AuditFilter implements ContainerRequestFilter {
     protected Logger logger = LoggerFactory.getLogger(this.getClass().getName());
     protected Gson gson = JsonUtil.getGson();
     protected static String uri = null;
-    protected static List<String> fieldsToScrub = new ArrayList<String>();
+    protected static List<String> fieldsToScrub = null;
     protected static String AUDIT_PATH = "/audit";
 
     static {
-        props = new Properties();
-        ClassPathResource cpr = new ClassPathResource("athena-audit.properties");
         try {
-            InputStream in = cpr.getInputStream();
-            props.load(in);
-            in.close();
-            uri = "http://" + props.getProperty("audit.hostname") + ":" + props.getProperty("audit.port") + "/" + props.getProperty("audit.componentName") + "/";
+            Configuration props = new PropertiesConfiguration("audit-client.properties");
+            uri = "http://" + props.getString("audit.hostname") + ":"
+                            + props.getString("audit.port") + "/"
+                            + props.getString("audit.componentName") + "/";
+            fieldsToScrub = props.getList("audit.fieldsToScrub");
             ClientConfig cc = new DefaultClientConfig();
             Client c = Client.create(cc);
             component = c.resource(uri);
 
-        } catch (Exception e) {
-            Logger log2 = LoggerFactory.getLogger(AuditFilter.class);
-            log2.error(e.getMessage(), e);
+        } catch (ConfigurationException e) {
+            Logger tempLog = LoggerFactory.getLogger(AuditFilter.class);
+            tempLog.error(e.getMessage(), e);
         }
-
-        fieldsToScrub.add("cardNumber");
-        fieldsToScrub.add("cvv");
-        fieldsToScrub.add("expirationDate");
     }
 
     @Override
