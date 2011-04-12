d9270b9934e35edf29cca9d286f30a2f21e141be
Gary Moore <gary.moore@fracturedatlas.org>
2011-04-12 10:58:31 -0400

    Removed changes to core layer, duplicated changes in another branch

---
+++ helpers/codes/src/main/java/org/fracturedatlas/athena/helper/codes/model/Code.java
@@ -0,0 +1,105 @@
+/*
+
+ATHENA Project: Management Tools for the Cultural Sector
+Copyright (C) 2010, Fractured Atlas
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/
+
+*/
+
+package org.fracturedatlas.athena.helper.codes.model;
+
+import java.util.Date;
+import java.util.Set;
+import org.fracturedatlas.athena.client.PTicket;
+
+
+public class Code {
+    String id;
+    String code;
+    String description;
+    Date startDate;
+    Date endDate;
+    Set<String> eligibleClients;
+    Set<String> tickets;
+    Boolean enabled;
+
+    public Code() {
+
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<String> getEligibleClients() {
+        return eligibleClients;
+    }
+
+    public void setEligibleClients(Set<String> eligibleClients) {
+        this.eligibleClients = eligibleClients;
+    }
+
+    public Set<String> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(Set<String> tickets) {
+        this.tickets = tickets;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
