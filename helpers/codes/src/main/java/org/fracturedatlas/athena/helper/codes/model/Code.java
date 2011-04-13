e0f3b35beac9443b079e21c6102297055f19bdce
Gary Moore <gary.moore@fracturedatlas.org>
2011-04-13 17:00:25 -0400

    CodeManager tests

--- helpers/codes/src/main/java/org/fracturedatlas/athena/helper/codes/model/Code.java
+++ helpers/codes/src/main/java/org/fracturedatlas/athena/helper/codes/model/Code.java
@@ -20,23 +20,88 @@ along with this program.  If not, see <http://www.gnu.org/licenses/
 
 package org.fracturedatlas.athena.helper.codes.model;
 
+import java.text.ParseException;
 import java.util.Date;
 import java.util.Set;
 import java.util.TreeSet;
+import org.fracturedatlas.athena.client.PTicket;
+import org.fracturedatlas.athena.id.IdAdapter;
+import org.fracturedatlas.athena.util.date.DateUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class Code {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+    
     String id;
     String code;
     String description;
     Date startDate;
     Date endDate;
+    Integer price;
     Set<String> eligibleClients;
     Set<String> tickets;
+    Set<String> performances;
+    Set<String> events;
     Boolean enabled;
 
     public Code() {
         tickets = new TreeSet<String>();
+        performances = new TreeSet<String>();
+        events = new TreeSet<String>();
+    }
+
+    public Code (PTicket pTicket) {
+        this();
+
+        if(pTicket == null) {
+            return;
+        }
+
+        if(pTicket.getId() != null) {
+            this.id = IdAdapter.toString(pTicket.getId());
+        }
+
+        if(pTicket.get("codeEnabled") != null) {
+            this.enabled = Boolean.parseBoolean(pTicket.get("codeEnabled"));
+        }
+
+        this.description = pTicket.get("codeDescription");
+        this.code = pTicket.get("code");
+
+        if(pTicket.get("codeStartDate") != null) {
+            try {
+                this.startDate = DateUtil.parseDate(pTicket.get("codeStartDate"));
+            } catch (ParseException ignored) {
+                logger.info(ignored.getMessage());
+            }
+        }
+
+        if(pTicket.get("codeEndDate") != null) {
+            try {
+                this.endDate = DateUtil.parseDate(pTicket.get("codeEndDate"));
+            } catch (ParseException ignored) {
+                logger.info(ignored.getMessage());
+            }
+        }
+    }
+
+    public Set<String> getEvents() {
+        return events;
+    }
+
+    public void setEvents(Set<String> events) {
+        this.events = events;
+    }
+
+    public Set<String> getPerformances() {
+        return performances;
+    }
+
+    public void setPerformances(Set<String> performances) {
+        this.performances = performances;
     }
 
     public String getCode() {
@@ -101,5 +166,34 @@ public class Code {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+    
+    public PTicket toRecord() {
+        PTicket pTicket = new PTicket();
+        pTicket.put("id", id);
+
+        if(enabled != null) {
+            pTicket.put("codeEnabled", Boolean.toString(enabled));
+        }
+
+        pTicket.put("codeDescription", description);
+        pTicket.put("code", code);
+        
+        if(startDate != null) {
+            pTicket.put("codeStartDate", DateUtil.formatDate(startDate));
+        }
+        if(startDate != null) {
+            pTicket.put("codeEndDate", DateUtil.formatDate(endDate));
+        }
+
+        return pTicket;
     }
 }
